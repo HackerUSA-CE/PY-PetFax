8f4c8143386b9ec94dc1e94acf529f940d5a49ce
Jerrica Bobadilla <jerrica@Jerricas-iMac-5.home>
2021-09-08 10:33:11 -0700

    add part 2 solution

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-# config                    
-from flask import Flask
-app = Flask(__name__)
-
-# index route
-@app.route('/')
-def index(): 
-    return 'Hello, PetFax!'
-
-# pets index route
-@app.route('/pets')
-def pets(): 
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
---
+++ petfax/__init__.py
@@ -0,0 +1,14 @@
+# config                    
+from flask import Flask
+
+# factory
+def create_app():
+    app = Flask(__name__)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # return the app 
+    return app
