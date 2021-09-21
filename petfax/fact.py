30a6e8696bf55509017a2eaa2833a7bd5cbbb8ee
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-09-21 15:25:40 -0700

    add solution

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,6 +1,14 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/', methods=['GET', 'POST'])
+def index(): 
+    if request.method == 'POST':
+        print(request.form)
+        return redirect('/facts')
+    
+    return render_template('facts/index.html')
 
 @bp.route('/new')
 def new(): 
