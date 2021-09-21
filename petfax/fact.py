711562fd3ece79c3635b5bb7033d5c647697b719
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-09-21 15:18:39 -0700

    fix wording

--- petfax/fact.py
+++ petfax/fact.py
@@ -2,6 +2,6 @@ from flask import ( Blueprint, render_template )
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/')
-def index(): 
-    return render_template('facts/index.html')
+@bp.route('/new')
+def new(): 
+    return render_template('facts/new.html')
