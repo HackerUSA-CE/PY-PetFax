3be56fe1be800dbc94b31eeeea002874246a5571
Jerrica Bobadilla <jerrica@Jerricas-iMac.home>
2021-10-04 14:59:12 -0700

    add soln

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/petfax'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
     
     from . import models 
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,14 +1,23 @@
 from flask import ( Blueprint, render_template, request, redirect ) 
+from . import models 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter, fact=fact) 
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
         return redirect('/facts')
+
+    results = models.Fact.query.all()
     
-    return render_template('facts/index.html')
+    return render_template('facts/index.html', facts=results)
 
 @bp.route('/new')
 def new(): 
