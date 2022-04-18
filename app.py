29f5464637991b2e6db4e2c790d056eee8764e87
Aaron Lu <aaronnoh0409@gmail.com>
2022-04-18 18:40:11 -0400

    added two routes

---
+++ app.py
@@ -0,0 +1,13 @@
+# config
+from flask import Flask
+app = Flask(__name__)
+
+# index route
+@app.route('/')
+def index(): 
+    return 'Hello, this is PetFax!'
+
+# pets index route
+@app.route('/pets')
+def pets():
+    return 'These are our pets available for adoption!'
