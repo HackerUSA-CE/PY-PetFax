bf73427490f4ede94bdb3e766d8b9bcd6589f4c9
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-04-20 17:32:09 -0400

    PY-petfax-app

---
+++ petfax/__init__.py
@@ -0,0 +1,23 @@
+# config                    
+from flask import Flask
+
+# factory
+def create_app():
+    app = Flask(__name__)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+
+    # return the app 
+    return app
+
+
+ 
+
---
+++ petfax/pet.py
@@ -0,0 +1,14 @@
+from flask import ( Blueprint, render_template ) 
+import json 
+
+pets = json.load(open('pets.json'))
+print(pets)
+
+bp = Blueprint('pets', __name__, url_prefix="/pets")
+
+@bp.route('/')
+def index(): 
+    return render_template('index.html', pets='pets') 
+
+
+
