5c959089de0380fcedab45137d64aef8ddaea6dd
Aaron Lu <aaronnoh0409@gmail.com>
2022-04-20 21:09:54 -0400

    flask migrate test worked!

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,21 @@
+from . import models
 from flask import Flask
-
+from flask_migrate import Migrate 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:YOUR_PASSWORD@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    # index route
+    @app.route('/')
+    def index():
+        return 'Hello, PetFax!'
 
     from . import main
     app.register_blueprint(main.bp)
@@ -10,4 +23,9 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app
     return app
