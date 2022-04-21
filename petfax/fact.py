4aaabff67779dc2ff63b0b5500cd746b97d957a1
Aaron Lu <aaronnoh0409@gmail.com>
2022-04-20 21:24:24 -0400

    checkpoint

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -6,10 +7,20 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
+
         return redirect('/facts')
 
-    return render_template('facts/index.html')
+    results = models.Fact.query.all()
+    for result in results:
+        print(result)
+
+    return render_template('facts/index.html', facts=results)
 
 
 @bp.route('/new')
