d908648852e7c22a0004ee7116105082354e45c9
Venkatesh Bhamidipati <vbhamidipati@yahoo.com>
2022-04-24 17:47:00 -0400

    PY-petfax-app

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,8 +1,6 @@
 from flask import ( Blueprint, render_template ) 
 import json
-import jinja2
 
-jinja_environment = jinja2.Environment(autoescape=True, loader=jinja2.FileSystemLoader('templates'))
 
 pets = json.load(open('pets.json'))
 print(pets)
