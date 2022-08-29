637dde83ec962f672e0e89b7a59e5a597a81c8c7
AJ Phoenix <ajph03nix@gmail.com>
2022-08-29 18:30:31 -0400

    just pushing

---
+++ petfax/ __init__.py
@@ -0,0 +1,22 @@
+# config                    
+from flask import Flask
+#factory
+def create_app():
+    app = Flask(__name__)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, this is PetFax!'
+
+    # pets index route
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
+    return app
