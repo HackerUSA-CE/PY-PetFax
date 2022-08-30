0c33372020cfeeba3f4d612307c2dae8117b2854
AJ Phoenix <ajph03nix@gmail.com>
2022-08-30 19:35:25 -0400

    Finished add post new route and database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,22 +1,38 @@
 # config                    
 from flask import Flask
+from flask_migrate import Migrate
+#factory
 
 # factory
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+    from . import models
+
+    models.db.init_app(app)
+    migrate = Migrate(app,models.db)
+
     # index route
     @app.route('/')
     def index(): 
-        return 'Hello, PetFax!'
+        return 'Hello, this is PetFax!'
+        
 
+    # pets index route
     # register pet blueprint 
     from . import pet 
     app.register_blueprint(pet.bp)
 
-    # register fact blueprint 
+        # register fact blueprint 
     from . import fact
     app.register_blueprint(fact.bp)
 
-    # return the app 
-    return app
+
+    return app  
+
+
+
+
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,16 +1,25 @@
 from flask import ( Blueprint, render_template,request,redirect ) 
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
-
-@bp.route('/new',methods=['GET','POST'])
-def new(): 
-    print(request.form)
-    return render_template('facts/new.html')
 
 @bp.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = models.Fact(submitter=submitter,fact = fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
         print(request.form)
         return redirect('/facts')
-    
-    return "thank you for submitting"
+
+    results = models.Fact.query.all()
+    for result in results:
+        print(result)
+    return render_template('facts/index.html', facts=results)
+
+@bp.route('/new', methods=['GET','POST'])
+def new(): 
+    return render_template('facts/new.html')
