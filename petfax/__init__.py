ad45c935108c7cc1cf7f143ed8cc4b5744464866
AriesRedWar <philipjbowman5@gmail.com>
2022-12-11 13:09:28 -0500

    static and styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,13 +1,22 @@
-from flask import Flask 
+# config                    
+from flask import Flask
 
-def create_app(): 
+# factory
+def create_app():
     app = Flask(__name__)
 
+    # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
 
-    from . import pet
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
 
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
     return app
