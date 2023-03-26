4682602d619986118e9440f4217e80f5c7af15b5
Thomas Davidson <tomd1969@gmail.com>
2023-03-26 13:17:53 -0400

    Completed activity 3

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
