8cd29fd557b1ae5c52a102fcedb892d3b68e0845
leslieportiz <lorraineportiz@gmail.com>
2023-06-05 00:53:45 -0700

    Completed static files and styling task.

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -11,8 +11,9 @@ def create_app():
         return 'Hello, PetFax!'
 
     # register pet blueprint 
-    from . import pet 
+    from . import pet, fact
     app.register_blueprint(pet.bp)
+    app.register_blueprint(fact.bp)
 
     # return the app 
     return app
---
+++ petfax/fact.py
@@ -0,0 +1,8 @@
+from flask import ( Blueprint, render_template ) 
+import json 
+
+bp = Blueprint('fact', __name__, url_prefix="/facts")
+
+@bp.route('/new')
+def new(): 
+    return render_template('facts/new.html')
