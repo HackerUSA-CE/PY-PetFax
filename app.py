e49628d2483a1f140a77efd47e1b8a1522122156
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-18 20:02:50 -0700

    reorganized app route files

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return 'Hello, this is PetFax!'
-
-@app.route('/pets')
-def pets():
-  return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
