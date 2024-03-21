ae3d1b784487a2ac45ede379e45ebfc198f01da3
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-20 19:49:25 -0700

    static folders changes

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static',static_folder='static')
 
     @app.route('/')
     def index(): 
