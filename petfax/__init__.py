99321f2e7898cc76cdbd992420783a55f04430c1
Chris <117781333+chrism-la@users.noreply.github.com>
2024-03-20 19:16:22 -0700

    static folder

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,16 @@
-from flask import Flask 
+from flask import Flask
 
-def create_app(): 
-
+def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello(): 
-        return 'Hello, PetFax!'
+    def index(): 
+        return 'Hello, PetFax!' 
     
-    from . import pet
+    from . import pet 
     app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     return app
